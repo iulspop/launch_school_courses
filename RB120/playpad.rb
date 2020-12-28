1dfdc384abcc78f73b305715136dde618686be2b
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-12-28 10:13:11 -0500

    edit playpad

--- RB120/playpad.rb
+++ RB120/playpad.rb
@@ -1,6 +1,9 @@
-The application lets a customer place an order for a meal.
+=begin
+a customer place an order for a meal.
 A meal always has three meal items: a burger, a side, and drink.
 For each meal item, the customer must choose an option.
 The application must compute the total cost of the order.
 
 Write an application from this description.
+
+=end
