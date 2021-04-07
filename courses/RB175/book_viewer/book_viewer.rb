c0d6bb7d3db816e78427a3734a1df49ac24c4bb1
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-04-07 07:20:03 -0400

    Add route and logic for /chapter/1 path

--- courses/RB175/book_viewer/book_viewer.rb
+++ courses/RB175/book_viewer/book_viewer.rb
@@ -6,3 +6,11 @@ get "/" do
   @chapter_titles = File.readlines("data/toc.txt")
   erb :home
 end
+
+get "/chapters/1" do
+  @title = "Chapter 1" 
+  @chapter_titles = File.readlines("data/toc.txt")
+  @chapter = File.readlines("data/chp1.txt")
+
+  erb :chapter
+end
