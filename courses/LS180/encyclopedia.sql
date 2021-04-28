12f34bb2b07b5787876e73fe9cfeb810075bf918
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-04-28 10:24:00 -0400

    Quick commit

--- courses/LS180/encyclopedia.sql
+++ courses/LS180/encyclopedia.sql
@@ -35,3 +35,21 @@ ALTER TABLE animals ALTER COLUMN max_weight_kg TYPE decimal(10, 4);
 
 ALTER TABLE animals ADD CONSTRAINT unique_binomial_name UNIQUE(binomial_name);
 
+INSERT INTO countries (name, capital, population) VALUES ('France', 'Paris', 67158000);
+
+INSERT INTO countries (name, capital, population)
+  VALUES ('USA', 'Washington D.C.', 325365189),
+         ('Germany', 'Berlin', 82349400),
+         ('Japan', 'Tokyo', 126672000);
+
+INSERT INTO celebrities (first_name, last_name, occupation, date_of_birth, deceased)
+  VALUES ('Bruce', 'Springsteen', 'Singer, Songwriter', '1949-09-23', false),
+         ('Scarlett', 'Johansson', 'Actress', '1984-11-22', DEFAULT),
+         ('Frank', 'Sinatran', 'Singer', '1915-12-12', true),
+         ('Tom', 'Cruise', 'Actor', '1962-07-03', DEFAULT);
+
+ALTER TABLE celebrities ALTER COLUMN last_name DROP NOT NULL;
+
+INSERT INTO celebrities (first_name, last_name, occupation, date_of_birth, deceased)
+  VALUES ('Madonna', NULL, 'Singer, Actress', '1958-09-23', DEFAULT),
+         ('Prince', NULL, 'Singer, Songwriter, Musician, Actor', '1958-11-22', true),
