48cb89d84c0f37433acc5f649ca2eb794a31786f
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-05-06 08:12:37 -0400

    Quick commit

--- courses/LS180/animals.sql
+++ courses/LS180/animals.sql
@@ -11,3 +11,22 @@ INSERT INTO birds (name, age, species)
          ('Jennifer', 3, 'Magpie'),
          ('Jamie', 4, 'Owl'),
          ('Roy', 8, 'Crow');
+
+SELECT * FROM birds;
+
+SELECT * FROM birds WHERE age < 5;
+
+UPDATE birds SET species = 'Raven' WHERE name = 'Roy';
+
+UPDATE birds SET species = 'Hawk' WHERE name = 'Jamie';
+
+DELETE FROM birds WHERE name = 'Charlie';
+
+ALTER TABLE birds ADD CONSTRAINT age_is_positive_check CHECK (age > 0);
+
+-- INSERT INTO birds (name, age, species) VALUES ('h', -10, 'h');
+
+SELECT * FROM birds;
+
+DROP TABLE birds;
+
