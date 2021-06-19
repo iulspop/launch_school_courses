214d6f4b298b5a12a7ee3247ee56f5d984d3112f
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-06-19 19:58:03 -0400

    Add age & greeter js exercises

---
+++ courses/JS210/intro_to_js_book/2_variables/age.js
@@ -0,0 +1,4 @@
+let age = 22;
+[10, 20, 30, 40].forEach((years) => {
+  console.log(`In ${years} years, you will be ${years + age} years old.`)
+});
---
+++ courses/JS210/intro_to_js_book/2_variables/greeter.js
@@ -0,0 +1,5 @@
+let first_name = "Victor";
+
+["Morning", "Afternoon", "Evening"].forEach((greeting) => {
+  console.log(`Good ${greeting}, ${first_name}.`)
+})
