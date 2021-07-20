dc29b35c57a39c22f3eb36bce179e6ae752602e8
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-07-20 10:08:34 -0400

    Solve isValidEmail LS215

---
+++ courses/LS215/isValidEmail.js
@@ -0,0 +1,28 @@
+function isValidEmail(email) {
+  const regex = multilineRegex(`
+    ^
+      [a-z0-9]+
+      @
+      ([a-z]+\\.)+
+      [a-z]+
+    $
+  `, 'i')
+
+  return regex.test(email)
+}
+
+function multilineRegex(string, flags = '') {
+  return new RegExp(string.replace(/[ \n]/g, ''), flags)
+}
+
+console.log(isValidEmail('Foo@baz.com.ph'));          // returns true
+console.log(isValidEmail('Foo@mx.baz.com.ph'));       // returns true
+console.log(isValidEmail('foo@baz.com'));             // returns true
+console.log(isValidEmail('foo@baz.ph'));              // returns true
+console.log(isValidEmail('HELLO123@baz'));            // returns false
+console.log(isValidEmail('foo.bar@baz.to'));          // returns false
+console.log(isValidEmail('foo@baz.'));                // returns false
+console.log(isValidEmail('foo_bat@baz'));             // returns false
+console.log(isValidEmail('foo@bar.a12'));             // returns false
+console.log(isValidEmail('foo_bar@baz.com'));         // returns false
+console.log(isValidEmail('foo@bar.....com'));         // returns false
