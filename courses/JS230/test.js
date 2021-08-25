19d231f225da90d27742a7a8a525244bcd31460f
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-08-25 08:43:36 -0400

    Quick commit

---
+++ courses/JS230/test.js
@@ -0,0 +1,21 @@
+console.log(document.querySelector("input[name='email']"));
+
+timesCalled = 0;
+function walk(node) {
+  ++timesCalled;
+  console.log(node.nodeName);
+  if (node.childNodes.length === 0) {
+    return;
+  }
+  each(node.childNodes, walk);
+}
+
+function each(collection, fn) {
+  for (let i = 0; i < collection.length; i++) {
+    const element = collection[i];
+    fn(element);
+  }
+}
+
+// walk(document);
+// console.log(timesCalled);
