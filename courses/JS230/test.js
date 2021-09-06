c6af941ddeb1320d08a7a9057c23383c8b3181f1
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-09-06 17:49:13 -0400

    Traverse DOM to find scrollable elements

--- courses/JS230/test.js
+++ courses/JS230/test.js
@@ -1,21 +1,20 @@
-console.log(document.querySelector("input[name='email']"));
-
-timesCalled = 0;
-function walk(node) {
-  ++timesCalled;
-  console.log(node.nodeName);
-  if (node.childNodes.length === 0) {
-    return;
-  }
-  each(node.childNodes, walk);
-}
-
-function each(collection, fn) {
-  for (let i = 0; i < collection.length; i++) {
-    const element = collection[i];
-    fn(element);
+function createWalk(check) {
+  let found = []
+  return function walk(node) {
+    if (check(node)) { found.push(node) }
+    if (node.childNodes.length === 0) { return found }
+    for (let i = 0; i < node.childNodes.length; i++) {
+      const element = node.childNodes[i]
+      walk(element)
+    }
+    return found
   }
 }
 
-walk(document);
-console.log(timesCalled);
+function isScrollable(element) {
+  return element.scrollHeight > element.clientHeight
+}
+
+let results = createWalk(isScrollable)(document.body);
+
+console.log(results)
