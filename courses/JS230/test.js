ed16f7852b3c0dc77c3e4e6b51907805d05942f7
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-09-27 11:58:07 -0400

    Add notes for declarative DOM manipulation

--- courses/JS230/test.js
+++ courses/JS230/test.js
@@ -17,4 +17,23 @@ function isScrollable(element) {
 
 let results = createWalk(isScrollable)(document.body);
 
-console.log(results)
+/*
+take old tree => intermediate reperesntation (Virtual DOM)
+  is intermediate representation inneficient unnecesary?
+take new tree => intermediate repesentation (Virtual DOM)
+
+declarativeRender(component)
+  knows old Virtual DOM from last render
+  creates new Virtual DOM
+  compare to know which changed
+  go over actual DOM, and 
+
+map === the boolean value
+  how does react make diff between identity and equality?
+  - key? whatever is given to render, list a post => the post id
+  - structural identification?
+  - simple solution: assign unique num for each node
+
+take old tree, walk, when boolean true,
+take value from new tree and replace old value.
+*/
